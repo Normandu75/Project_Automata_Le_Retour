8b9cc250fbf8b3da07ffb8422f8d7472e7bb2125
HO2PTS <100843102+HO2PTS@users.noreply.github.com>
2026-01-30 14:51:59 +0100

    PortalManager
    Téléportation du joueur dans le portail

--- Assets/Mini_Game/Script/Version_Finale/Portals/Script/PortalManager.cs
+++ Assets/Mini_Game/Script/Version_Finale/Portals/Script/PortalManager.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PortalManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform APos; //create APos Object on Portal 1
+    public Transform BPos; //create APos Object on Portal 2
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider col)
     {
-        
+        if (col.CompareTag("Portal 1"))
+        {
+            PlayerMovementTutorial PM = GetComponent<PlayerMovementTutorial>();
+            PM.enabled = false; 
+            transform.position = BPos.transform.position;
+            transform.rotation = new Quaternion(transform.rotation.x, BPos.rotation.y, transform.rotation.z, transform.rotation.w); 
+            PM.enabled = true; 
+        }
+
+        if (col.CompareTag("Portal 2"))
+        {
+            PlayerMovementTutorial PM = GetComponent<PlayerMovementTutorial>();
+            PM.enabled = false; 
+            transform.position = APos.transform.position;
+            transform.rotation = new Quaternion(transform.rotation.x, APos.rotation.y, transform.rotation.z, transform.rotation.w); 
+            PM.enabled = true; 
+        }
     }
 }
