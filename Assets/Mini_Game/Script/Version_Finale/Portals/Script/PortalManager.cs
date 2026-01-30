78b21fa222dd5e9a8f2cddb6ff17dee786892aa6
HO2PTS <100843102+HO2PTS@users.noreply.github.com>
2026-01-30 15:29:26 +0100

    Portail Intégration
    Intégration des Portails, des TP, À faire dans ma branch:
    Debug collision du joueur bloqué dans le TP.

--- Assets/Mini_Game/Script/Version_Finale/Portals/Script/PortalManager.cs
+++ Assets/Mini_Game/Script/Version_Finale/Portals/Script/PortalManager.cs
@@ -1,27 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class PortalManager : MonoBehaviour
 {
     public Transform APos; //create APos Object on Portal 1
     public Transform BPos; //create APos Object on Portal 2
+    public PlayerMovementTutorial PM;
+    private float delayTime = 5f;
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.CompareTag("Portal 1"))
+        /* IEnumerator DelayAction(float delayTime)
         {
-            PlayerMovementTutorial PM = GetComponent<PlayerMovementTutorial>();
+            yield return new WaitForSeconds(delayTime);
+        } */ // COROUTINE non fonctionnelle. 
+
+        if (col.CompareTag("Portal A"))
+        {
+            PM = GetComponent<PlayerMovementTutorial>();
             PM.enabled = false; 
             transform.position = BPos.transform.position;
             transform.rotation = new Quaternion(transform.rotation.x, BPos.rotation.y, transform.rotation.z, transform.rotation.w); 
+             // COROUTINE non fonctionnelle. StartCoroutine(DelayAction(delayTime));
             PM.enabled = true; 
+            
         }
 
-        if (col.CompareTag("Portal 2"))
+        if (col.CompareTag("Portal B"))
         {
-            PlayerMovementTutorial PM = GetComponent<PlayerMovementTutorial>();
+            PM = GetComponent<PlayerMovementTutorial>();
             PM.enabled = false; 
             transform.position = APos.transform.position;
             transform.rotation = new Quaternion(transform.rotation.x, APos.rotation.y, transform.rotation.z, transform.rotation.w); 
+            // COROUTINE non fonctionnelle.  StartCoroutine(DelayAction(delayTime));
             PM.enabled = true; 
         }
     }
